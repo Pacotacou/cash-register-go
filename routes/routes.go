ad6cfc72e3b2213c3bef0b62f604794b0dfa476f
Pacotacou <p4c0taco@gmail.com>
2025-03-12 20:25:09 -0500

    feat: works

--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,16 @@ import (
 func RegisterRoutes() *mux.Router {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", homeHandler).Methods("GET")
+	// Serve static files from the "public" and "styles" directories
+	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+	r.PathPrefix("/styles/").Handler(http.StripPrefix("/styles/", http.FileServer(http.Dir("./styles"))))
+
+	// Serve the HTML file for the root URL
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./views/cash_register.html")
+	}).Methods("GET")
+
+	// API routes
 	r.HandleFunc("/ping", pingHandler).Methods("GET")
 	r.HandleFunc("/end-day", controllers.EndDayHandler).Methods("POST")
 
@@ -18,11 +27,6 @@ func RegisterRoutes() *mux.Router {
 }
 
 // Handlers
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Welcome to Cash Register API"))
-}
-
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("pong"))
