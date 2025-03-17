df498bff20c59e5dd691a72f8c4dac3e44779c22
Pacotacou <p4c0taco@gmail.com>
2025-03-16 22:04:01 -0500

    feat: browser opening

--- server.go
+++ server.go
@@ -6,12 +6,25 @@ import (
 	"net/http"
 
 	"cash-register/routes"
+
+	"github.com/gorilla/mux"
+	"github.com/pkg/browser"
 )
 
 func main() {
 	r := routes.RegisterRoutes()
 
-	// Start server
+	go startServer(r)
+
+	err := browser.OpenURL("http://localhost:8080")
+
+	if err != nil {
+		fmt.Println("Error opening the browser: ", err)
+	}
+
+}
+
+func startServer(r *mux.Router) {
 	port := ":8080"
 	fmt.Println("Server is running on port", port)
 	log.Fatal(http.ListenAndServe(port, r))
