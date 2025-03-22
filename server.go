83dc1ad0f88b5461e3f0d06e37b5eeadd6a94581
Pacotacou <p4c0taco@gmail.com>
2025-03-22 16:12:05 -0500

    feat: add dockerfile

--- server.go
+++ server.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"cash-register/routes"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync"
-
-	"cash-register/routes"
 
 	"github.com/gorilla/mux"
 	"github.com/pkg/browser"
@@ -14,19 +14,25 @@ import (
 
 func main() {
 	r := routes.RegisterRoutes()
-
 	var wg sync.WaitGroup
-
 	wg.Add(1)
+
+	// Check if we're running in a container
+	inContainer := os.Getenv("IN_CONTAINER")
+
 	go startServer(r, &wg)
 
-	err := browser.OpenURL("http://localhost:8080")
-
-	if err != nil {
-		fmt.Println("Error opening the browser: ", err)
+	// Only open the browser if we're not in a container
+	if inContainer != "true" {
+		err := browser.OpenURL("http://localhost:8080")
+		if err != nil {
+			fmt.Println("Error opening the browser: ", err)
+		}
+	} else {
+		fmt.Println("Running in container. Access the application at http://localhost:8080 from your host machine.")
 	}
-	wg.Wait()
 
+	wg.Wait()
 }
 
 func startServer(r *mux.Router, wg *sync.WaitGroup) {
